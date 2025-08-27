cd6b5f96a71278f833c86346938d3c3ef9ebb72c
Jaroslav Makeichyk <jaroslau1@gmail.com>
2025-08-27 11:21:20 +0200

    feat: complete reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const a = n < 0 ? n * -1 : n;
+  return a.toString().split('').reverse().join('');
 };
